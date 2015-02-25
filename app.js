9b7384a89d729e87cd04d9e688711d7d364402fa
Orkhan Huseynov <huseynov.orkhan@gmail.com>
2015-02-25 12:38:04 +0400

    Minor changes
    Some syntax changes using JSHint

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-// "use strict";
+/*jslint node: true */
+'use strict';
 //Server create
 var http = require('http');
-var formatDate = require(__dirname + "/functions/formatDate");
-var getContent = require(__dirname + "/functions/getContent");
+var formatDate = require(__dirname + '/functions/formatDate');
+var getContent = require(__dirname + '/functions/getContent');
 
 
-var server = http.createServer(function (request, response) {
-  if (request.method === "GET"){
+http.createServer(function(request, response) {
+  if (request.method === 'GET') {
     getContent(request.url, response, __dirname);
   }
-  request.on('end', function(){
-    console.log(formatDate(new Date()) + "\tReguest:\t" + request.method + " URL:" + request.url);
+  request.on('end', function() {
+    console.log(formatDate(new Date()) + '\tReguest:\t' + request.method + ' URL:' + request.url);
 
   });
 }).listen(80);
@@ -18,23 +19,24 @@ console.log('Server running at http://127.0.0.1:80/');
 // ----------------------------------------------
 
 // Rates query
-var ratesIba = require(__dirname + "/functions/getRates/ratesIba");
-var ratesBanktechnique = require(__dirname + "/functions/getRates/ratesBankTechnique");
-var ratesKapitalBank = require(__dirname + "/functions/getRates/ratesKapitalBank");
-var ratesBankStandard = require(__dirname + "/functions/getRates/ratesBankStandard");
-var ratesBankOfBaku= require(__dirname + "/functions/getRates/ratesBankOfBaku");
+var ratesIba = require(__dirname + '/functions/getRates/ratesIba');
+var ratesBanktechnique = require(__dirname + '/functions/getRates/ratesBankTechnique');
+var ratesKapitalBank = require(__dirname + '/functions/getRates/ratesKapitalBank');
+var ratesBankStandard = require(__dirname + '/functions/getRates/ratesBankStandard');
+var ratesBankOfBaku = require(__dirname + '/functions/getRates/ratesBankOfBaku');
 
-var concatenateRates = require(__dirname + "/functions/concatenateRates");
+var concatenateRates = require(__dirname + '/functions/concatenateRates');
 
-function GetRates(){ 
+function GetRates() {
   var timestamp = formatDate(new Date());
   ratesBankOfBaku(timestamp);
-  ratesKapitalBank(timestamp);  
+  ratesKapitalBank(timestamp);
   ratesIba(timestamp);
   ratesBanktechnique(timestamp);
   ratesBankStandard(timestamp);
   concatenateRates();
 }
 
-setInterval(GetRates, 1000 * 60 * 10);
+GetRates();
 
+setInterval(GetRates, 1000 * 60 * 10);
