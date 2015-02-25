72d37956a54cb5627915fe71dba59eab4b1e320f
Orkhan Huseynov <huseynov.orkhan@gmail.com>
2015-02-25 19:29:39 +0400

    Optimized code for front end script
    Front end script has been optimised

--- public/js/script.js
+++ public/js/script.js
@@ -1,76 +1,64 @@
-"use strict";
-$(document).ready(function(){
-    $.getJSON( "/data/rates.json", function( obj ) {
-      var items = [];
-      var bankCode = "";
-      
-      $.each( obj.banktechnique[1], function( key, val ) {
-        $("#bt_"+key).text(val);
-      });
-      $.each( obj.iba[1], function( key, val ) {
-        $("#iba_"+key).text(val);
-      });
-      $.each( obj.kapitalbank[1], function( key, val ) {
-        $("#kb_"+key).text(val);
-      });    
-      $.each( obj.bankstandard[1], function( key, val ) {
-        $("#bs_"+key).text(val);
-      });     
-      $.each( obj.bankofbaku[1], function( key, val ) {
-        $("#bob_"+key).text(val);
-      });
+/*jslint jquery: true, asi: false*/
+'use strict';
+$(document).ready(function() {
+  $.getJSON('/data/rates.json', function(obj) {
 
-      var currency = ['usd','eur','gbp','rub'];
-
-      $.each(currency, function(index, value) {
-        var maxEl = {};
-        var max = 0;
-        $("td[id$=buy_"+value+"]").each(function(){
-          var $this = parseFloat( $(this).text() );
-          if ($this >= max){
-            max = $this;
-            maxEl = $(this);
-          }
-        })
-        maxEl.addClass('success');
+    var data = [];
+    $.each(obj, function(bank, obj) {
+      $.each(obj[1], function(key, val) {
+        $('#' + bank + '_' + key).text(parseFloat(val).toFixed(4));
+        data.push({
+          type: key,
+          value: parseFloat(val)
+        });
       });
-      $.each(currency, function(index, value) {
-        var maxEl = {};
-        var max = 100;
-        $("td[id$=buy_"+value+"]").each(function(){
-          var $this = parseFloat( $(this).text() );
-          if ($this < max){
-            max = $this;
-            maxEl = $(this);
-          }
-        })
-        maxEl.addClass('danger');
-      });
-      $.each(currency, function(index, value) {
-        var maxEl = {};
-        var max = 100;
-        $("td[id$=sell_"+value+"]").each(function(){
-          var $this = parseFloat( $(this).text() );
-          if ($this < max){
-            max = $this;
-            maxEl = $(this);
-          }
-        })
-        maxEl.addClass('success');
-      });
-      $.each(currency, function(index, value) {
-        var maxEl = {};
-        var max = 0;
-        $("td[id$=sell_"+value+"]").each(function(){
-          var $this = parseFloat( $(this).text() );
-          if ($this > max){
-            max = $this;
-            maxEl = $(this);
-          }
-        })
-        maxEl.addClass('danger');
-      });
-
     });
 
+    var type = ['buy', 'sell'];
+    var currency = ['usd', 'eur', 'gbp', 'rub'];
+    var maxArray = {};
+    var minArray = {};
+    var max = 0;
+    var min = 1000;
+    var classType = ['success', 'danger']
+
+    $.each(type, function(key, type) {
+      $.each(currency, function(key, currency) {
+        $.each(data, function(key, value) {
+          if (type + '_' + currency === value.type) {
+            if (value.value > max) max = maxArray[value.type] = value.value;
+            if (value.value < min && value.value !== 0) min = minArray[value.type] = value.value;
+          }
+        });
+        max = 0;
+        min = 1000;
+      });
+    });
+    // console.log(maxArray);
+    // console.log(minArray);
+
+    $.each(maxArray, function(index, value) {
+      $('td[id$=' + index + ']').each(function() {
+        if ($(this).text() === value.toFixed(4).toString()) {
+          if (index.match(/buy/)) {
+            $(this).addClass(classType[0]);
+          } else {
+            $(this).addClass(classType[1]);
+          }
+        }
+      });
+    });
+
+    $.each(minArray, function(index, value) {
+      $('td[id$=' + index + ']').each(function() {
+        if ($(this).text() === value.toFixed(4).toString()) {
+          if (index.match(/buy/)) {
+            $(this).addClass(classType[1]);
+          } else {
+            $(this).addClass(classType[0]);
+          }
+        }
+      });
+    });
+  });
 });
