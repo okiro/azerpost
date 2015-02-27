604eecca787eadb0174b3176b1076b2daa297256
Orkhan Huseynov <huseynov.orkhan@gmail.com>
2015-02-27 11:06:18 +0400

    Callback hell
    - changed concatenate function to avoid callback hell

--- functions/concatenateRates.js
+++ functions/concatenateRates.js
@@ -1,37 +1,47 @@
 /*jslint node: true, asi: true */
 'use strict';
+var fs = require('fs');
 module.exports = function concatenateRates() {
-	var fs = require('fs');
-	fs.unlink(__dirname + '/rates.json', function(err) {
+	deleteFile('/rates.json', deleteFile('/../public/data/rates.json', readDir));
+}
+
+
+function deleteFile(fileName, callback) {
+	fs.unlink(__dirname + fileName, function(err) {
 		if (err) console.log(err);
-		fs.unlink(__dirname + '/../public/data/rates.json', function(err) {
-			if (err) console.log(err);
-			fs.readdir(__dirname + '/data', function(err, files) {
+		if (typeof callback !== 'undefined') callback();
+	});
+}
+
+function readDir() {
+	fs.readdir(__dirname + '/data', function(err, files) {
+		if (err) throw err;
+		filesLoop(files);
+	});
+}
+
+function filesLoop(files) {
+	files.forEach(function(currentValue, index, array) {
+		if (require('path').extname(currentValue) === '.json') {
+			fs.readFile(__dirname + '/data/' + currentValue, function(err, data) {
 				if (err) throw err;
-				files.forEach(function(currentValue, index, array) {
-					if (require('path').extname(currentValue) === '.json') {
-						fs.readFile(__dirname + '/data/' + currentValue, function(err, data) {
+				fs.writeFile(__dirname + '/rates.json', data.toString(), {
+					flag: 'a'
+				}, function(err) {
+					if (err) throw err;
+					if (index === array.length - 1) {
+						fs.readFile(__dirname + '/rates.json', function(err, data) {
 							if (err) throw err;
-							fs.writeFile(__dirname + '/rates.json', data.toString(), {
-								flag: 'a'
+							data = data.toString().replace(/\}\{/g, ',');
+							fs.writeFile(__dirname + '/../public/data/rates.json', data, {
+								flag: 'w'
 							}, function(err) {
 								if (err) throw err;
-								if (index === array.length - 1) {
-									fs.readFile(__dirname + '/rates.json', function(err, data) {
-										if (err) throw err;
-										data = data.toString().replace(/\}\{/g, ',');
-										fs.writeFile(__dirname + '/../public/data/rates.json', data, {
-											flag: 'w'
-										}, function(err) {
-											if (err) throw err;
-										});
-									});
-								}
 							});
 						});
 					}
 				});
 			});
-		});
+		}
 	});
 }
